b4203e816d15497233860a6168d1069ed54e12fa
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-02-09 22:07:18 -0300

    navigating to initial position

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -13,20 +13,32 @@ const Map = () => {
   const [initialPosition, setInitialPosition] = useState<[number, number]>([
     0, 0,
   ]);
+  const [map, setMap] = useState<any>(null);
+
+  console.log(map);
 
   useEffect(() => {
+    if (!map) return;
+
     navigator.geolocation.getCurrentPosition((position) => {
       const { coords } = position;
       const { latitude, longitude } = coords;
+
+      const zoom = 12;
+      const duration = 3;
+
+      map.flyTo([latitude, longitude], zoom, { duration });
+
       setInitialPosition([latitude, longitude]);
     });
-  }, []);
+  }, [map]);
 
   return (
     <MapContainer
       style={{ height: "100%", width: "100%" }}
       center={initialPosition}
-      zoom={3}
+      zoom={13}
+      whenCreated={(map) => setMap(map)}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -41,14 +53,14 @@ const Map = () => {
 export default Map;
 
 const LocationMarker = () => {
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound: (location) => {
-      console.log("location found:", location);
-    },
-  });
+  // const map = useMapEvents({
+  //   click() {
+  //     map.locate();
+  //   },
+  //   locationfound: (location) => {
+  //     console.log("location found:", location);
+  //   },
+  // });
 
   return (
     <Marker position={[51.505, -0.09]}>
